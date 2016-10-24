2502395d3bb9fec255a646192c3fd880862c75af
Bojana Skarich <bojana.skarich@gmail.com>
2016-10-23 20:04:20 -0700

    Add a seeds.rb file that will load the db with hard coded values.

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_one :profile, dependent: :destroy
 	has_many :todo_lists, dependent: :destroy
+	# Implement a 1:many through relationship from User to ToDoItem by using the 1:many relationship from User to ToDoLists as a source. 
+	has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
 end
