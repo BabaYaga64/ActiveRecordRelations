302fe0446b6fd80b7e2998b6f6329c978d5254b1
Bojana Skarich <bojana.skarich@gmail.com>
2016-10-23 20:27:01 -0700

    Add custom validations to user and profile model classes.

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 	has_many :todo_lists, dependent: :destroy
 	# Implement a 1:many through relationship from User to ToDoItem by using the 1:many relationship from User to ToDoLists as a source. 
 	has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
+	# Define a validation for username to enforce that username be supplied by using a built-in validator
+	validates :username, presence: true
 end
